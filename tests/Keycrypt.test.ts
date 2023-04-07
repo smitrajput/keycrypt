bb5708ac5522875d6053c91a4973b8f2ed67fee5
smitrajput <smitrajputtd@gmail.com>
2023-04-07 23:02:40 +0530

    :tada:: test scaffold

--- tests/Keycrypt.test.ts
+++ tests/Keycrypt.test.ts
@@ -1,5 +1,30 @@
-// write hardhat test template to test Keycrypt.sol
+import { expect } from "chai";
+import { Wallet, Provider, Contract } from "zksync-web3";
+import * as hre from "hardhat";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 
+const RICH_WALLET_PK = "0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110";
 
+async function deployKeycrypt(deployer: Deployer): Promise<Contract> {
+  const artifact = await deployer.loadArtifact("Keycrypt");
+  return await deployer.deploy(artifact, ["Hi"]);
+}
 
+describe("Keycrypt", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const provider = Provider.getDefaultProvider();
 
+    const wallet = new Wallet(RICH_WALLET_PK, provider);
+    const deployer = new Deployer(hre, wallet);
+
+    const keycrypt = await deployKeycrypt(deployer);
+
+    expect(await keycrypt.greet()).to.eq("Hi");
+
+    const setGreetingTx = await keycrypt.setGreeting("Hola, mundo!");
+    // wait until the transaction is mined
+    await setGreetingTx.wait();
+
+    expect(await keycrypt.greet()).to.equal("Hola, mundo!");
+  });
+});
