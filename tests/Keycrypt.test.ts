0a6489b30dcae3fe039aef5edd880fbb58952630
smitrajput <smitrajputtd@gmail.com>
2023-04-10 18:10:27 +0530

    :heavy_plus_sign:: addToWhitelist() test

--- tests/Keycrypt.test.ts
+++ tests/Keycrypt.test.ts
@@ -7,11 +7,11 @@ const RICH_WALLET_PK = "0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38ed
 
 async function deployKeycrypt(deployer: Deployer): Promise<Contract> {
   const artifact = await deployer.loadArtifact("Keycrypt");
-  return await deployer.deploy(artifact, ["Hi"]);
+  return await deployer.deploy(artifact, ["0x640E35B6AfC3F6AD37bC14792536dA06e1C8cc19", "0xb2234c988cC1C4BE0b31fEbd2bA54BE61735f315", "0x213ce1554f6F96dB5CdAEa7D750F89Ab2Ae43294"]);
 }
 
 describe("Keycrypt", function () {
-  it("Should return the new greeting once it's changed", async function () {
+  it("should whitelist a list of addresses", async function () {
     const provider = Provider.getDefaultProvider();
 
     const wallet = new Wallet(RICH_WALLET_PK, provider);
@@ -19,12 +19,12 @@ describe("Keycrypt", function () {
 
     const keycrypt = await deployKeycrypt(deployer);
 
-    expect(await keycrypt.greet()).to.eq("Hi");
-
-    const setGreetingTx = await keycrypt.setGreeting("Hola, mundo!");
+    expect(await keycrypt.owner()).to.eq("0x640E35B6AfC3F6AD37bC14792536dA06e1C8cc19");
+    // whitelist USDC, USDT, and WETH
+    const addToWhitelistTx = await keycrypt.addToWhitelist(["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", "0xdAC17F958D2ee523a2206206994597C13D831ec7", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"]);
     // wait until the transaction is mined
-    await setGreetingTx.wait();
+    await addToWhitelistTx.wait();
 
-    expect(await keycrypt.greet()).to.equal("Hola, mundo!");
+    expect(await keycrypt.isWhitelisted("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")).to.equal(true);
   });
 });
