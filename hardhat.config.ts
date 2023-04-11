0f2e35d4212218b8ecae78564c8ba67a65b1139c
smitrajput <smitrajputtd@gmail.com>
2023-04-11 12:33:25 +0530

    :heavy_plus_sign: zkSync local-test-setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,23 @@ import '@matterlabs/hardhat-zksync-verify';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+// dynamically changes endpoints for local tests
+const zkSyncTestnet =
+  process.env.NODE_ENV == "test"
+    ? {
+        url: "http://localhost:3050",
+        ethNetwork: "http://localhost:8545",
+        zksync: true,
+      }
+    : {
+				url: 'https://zksync2-testnet.zksync.dev',
+				ethNetwork:
+					'https://eth-goerli.g.alchemy.com/v2/o0zPBQM7rdfOMk8or8Dmlq60oHZqtnqU', //'goerli', // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+				zksync: true,
+				verifyURL:
+					'https://zksync2-testnet-explorer.zksync.dev/contract_verification',
+      };
+
 module.exports = {
 	zksolc: {
 		version: '1.3.8',
@@ -12,18 +29,17 @@ module.exports = {
 			isSystem: true,
 		},
 	},
+
 	defaultNetwork: 'zkSyncTestnet',
 
 	networks: {
-		zkSyncTestnet: {
-			url: 'https://zksync2-testnet.zksync.dev',
-			ethNetwork:
-				'https://eth-goerli.g.alchemy.com/v2/o0zPBQM7rdfOMk8or8Dmlq60oHZqtnqU', //'goerli', // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
-			zksync: true,
-			verifyURL:
-				'https://zksync2-testnet-explorer.zksync.dev/contract_verification',
-		},
+		hardhat: {
+      zksync: true,
+    },
+    // load test network details
+		zkSyncTestnet,
 	},
+	
 	solidity: {
 		compilers: [
 			{
--- test/Keycrypt.test.ts
+++ test/Keycrypt.test.ts
@@ -3,7 +3,7 @@ import { Wallet, Provider, Contract } from "zksync-web3";
 import * as hre from "hardhat";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 
-const RICH_WALLET_PK = "0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110";
+const RICH_WALLET_PK = "0x3eb15da85647edd9a1159a4a13b9e7c56877c4eb33f614546d4db06a51868b1c";
 
 async function deployKeycrypt(deployer: Deployer): Promise<Contract> {
   const artifact = await deployer.loadArtifact("Keycrypt");
@@ -17,14 +17,18 @@ describe("Keycrypt", function () {
     const wallet = new Wallet(RICH_WALLET_PK, provider);
     const deployer = new Deployer(hre, wallet);
 
-    const keycrypt = await deployKeycrypt(deployer);
+    //check ETH balance of RIch Wallet
+    const ethBalance = await wallet.getBalance();
+    console.log("ETH Balance: ", ethBalance.toString());
 
-    expect(await keycrypt.owner()).to.eq("0x640E35B6AfC3F6AD37bC14792536dA06e1C8cc19");
+    // const keycrypt = await deployKeycrypt(deployer);
+
+    // expect(await keycrypt.owner()).to.eq("0x640E35B6AfC3F6AD37bC14792536dA06e1C8cc19");
     // whitelist USDC, USDT, and WETH
-    const addToWhitelistTx = await keycrypt.addToWhitelist(["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", "0xdAC17F958D2ee523a2206206994597C13D831ec7", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"]);
-    // wait until the transaction is mined
-    await addToWhitelistTx.wait();
+    // const addToWhitelistTx = await keycrypt.addToWhitelist(["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", "0xdAC17F958D2ee523a2206206994597C13D831ec7", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"]);
+    // // wait until the transaction is mined
+    // await addToWhitelistTx.wait();
 
-    expect(await keycrypt.isWhitelisted("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")).to.equal(true);
+    // expect(await keycrypt.isWhitelisted("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")).to.equal(true);
   });
 });
