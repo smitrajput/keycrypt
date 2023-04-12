bbac65e87e8ce309c5a04335a6cb958d4379687e
smitrajput <smitrajputtd@gmail.com>
2023-04-12 23:04:41 +0530

    :hammer:: wallet creation test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # keycrypt
 ### A hackproof crypto smart wallet, featuring: <br/>
-✅ native account-abstraction capabilities on zkSync <br/>
+✅ account-abstraction capabilities: native zkSync VS app-layer ethereum <br/>
 ✅ social recovery <br/>
 ✅ 1/1 multisig behaviour for whitelisted interactions <br/>
 ✅ 2/3 multisig behaviour for changing the current owner and non-whitelisted interactions <br/>
--- test/foundry/Keycrypt.t.sol
+++ test/foundry/Keycrypt.t.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../../contracts/Keycrypt.sol";
+import "../../contracts/ETH_Factory.sol";
+import "../../contracts/ETH_Keycrypt.sol";
 
 // interface INonceHolder {
 //     /// @dev Returns the deployment nonce for the accounts used for CREATE opcode.
@@ -15,36 +16,40 @@ interface AnyswapV6Router {
 }
 
 contract KeycryptTest is Test {
-    uint256 zkSyncFork;
-    Keycrypt public keycrypt;
+    uint256 mainnetFork;
+    ETH_Factory public factory;
+    ETH_Keycrypt public keycrypt;
     address public owner;
     address public guardian1;
     address public guardian2;
+    IEntryPoint public entryPoint = IEntryPoint(0x0576a174D229E3cFA37253523E645A78A0C91B57);
 
     function setUp() public {
-        // create zksync testnet fork and deploy Keycrypt.sol to it
-        zkSyncFork = vm.createFork('https://eth-goerli.g.alchemy.com/v2/o0zPBQM7rdfOMk8or8Dmlq60oHZqtnqU');
-        vm.selectFork(zkSyncFork);
+        // create eth mainnet fork and deploy ETH_Factory.sol to it
+        mainnetFork = vm.createFork('https://eth-mainnet.g.alchemy.com/v2/SJVS2ntWXSBZLjio560R-qMw-s3XaN14');
+        vm.selectFork(mainnetFork);
         owner = address(0x123);
         guardian1 = address(0x456);
         guardian2 = address(0x789);
-        keycrypt = new Keycrypt(owner, guardian1, guardian2);
+        factory = new ETH_Factory(entryPoint);
     }
 
-    function testAddToWhitelist() public {
+    function test_FactoryDotCreateAccount() public {
         // counter.increment();
         // assertEq(counter.number(), 1);
-        INonceHolder nonceHolder = INonceHolder(0x0000000000000000000000000000000000008003);
-        uint256 nonce = nonceHolder.getDeploymentNonce(0x7Bae133d541Ac388BCdF17C72436ebbb625F94bF);
-        console.log('Nonce: ', nonce);
-        // AnyswapV6Router nonceHolder = AnyswapV6Router(0xB44a9B6905aF7c801311e8F4E76932ee959c663C);
-        // address nonce = nonceHolder.mpc();
-        // console.log('Nonce: ', nonce);
+        // INonceHolder nonceHolder = INonceHolder(0x0000000000000000000000000000000000008003);
+        // uint256 nonce = nonceHolder.getDeploymentNonce(0x7Bae133d541Ac388BCdF17C72436ebbb625F94bF);
+        keycrypt = factory.createAccount(owner, guardian1, guardian2, 0);
+        console.log('keycrypt address:', address(keycrypt));
+        assertEq(keycrypt.owner(), owner);
+        //send 1 ETH to keycrypt
+        // vm.deal(address(keycrypt), 1 ether);
     }
 
-    function testSetNumber(uint256 x) public {
-        // counter.setNumber(x);
-        // assertEq(counter.number(), x);
+    function test_ETH_KeycryptDotAddToWhitelist() public {
+        console.log('keycrypt balance:', address(keycrypt).balance);
+        console.log('keycrypt owner:', keycrypt.owner());
+        // entryPoint.
     }
 }
 
