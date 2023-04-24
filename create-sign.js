69db93c9001728ec46982d4c154340d8368e57aa
smitrajput <smitrajputtd@gmail.com>
2023-04-24 20:18:34 +0530

    :heavy_plus_sign:: signature in addToWhitelist() test

---
+++ create-sign.js
@@ -0,0 +1,63 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const { ethers } = require('ethers');
+
+const privateKey1 = process.env.ZKS_PRIVATE_KEY;
+const privateKey2 = process.env.ZKS_PRIVATE_KEY_2;
+
+const userOp = {
+  sender: '0xaa49524B61e4AfC10c2202cB25040Da25401ae4E',
+  nonce: 1,
+  initCode: '0x',
+  callData: '0x0a4a015c7000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb4000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec70000000000000000000000006b175474e89094c44da98b954eedeac495271d0f',
+  callGasLimit: 1000000,
+  verificationGasLimit: 1000000,
+  preVerificationGas: 1000000,
+  maxFeePerGas: 43683902336,
+  maxPriorityFeePerGas: 60865874,
+  paymasterAndData: '0x',
+  signature: '0x',
+};
+
+function createSignature(userOp, privateKey) {
+  const wallet = new ethers.Wallet(privateKey);
+  const dataToSign = ethers.utils.solidityKeccak256(
+    [
+      'address',
+      'uint256',
+      'bytes',
+      'bytes',
+      'uint256',
+      'uint256',
+      'uint256',
+      'uint256',
+      'uint256',
+      'bytes',
+    ],
+    [
+      userOp.sender,
+      userOp.nonce,
+      userOp.initCode,
+      userOp.callData,
+      userOp.callGasLimit,
+      userOp.verificationGasLimit,
+      userOp.preVerificationGas,
+      userOp.maxFeePerGas,
+      userOp.maxPriorityFeePerGas,
+      userOp.paymasterAndData,
+    ]
+  );
+
+  return wallet.signMessage(ethers.utils.arrayify(dataToSign));
+}
+
+(async () => {
+  const signature1 = await createSignature(userOp, privateKey1);
+  const signature2 = await createSignature(userOp, privateKey2);
+
+  console.log('Signature1:', signature1);
+  console.log('Signature2:', signature2);
+
+  const concatenatedSignatures = signature1 + signature2.slice(2); // Remove the '0x' from signature2 before concatenating
+  console.log('Concatenated Signatures:', concatenatedSignatures);
+})();
--- test/foundry/Keycrypt.t.sol
+++ test/foundry/Keycrypt.t.sol
@@ -20,14 +20,14 @@ contract KeycryptTest is Test {
         // create eth mainnet fork and deploy ETH_Factory.sol to it
         mainnetFork = vm.createFork('https://eth-mainnet.g.alchemy.com/v2/BKt4FdcCBCJR7b5-KAdqNfoovPA7rFcx');
         vm.selectFork(mainnetFork);
-        owner = address(0x123);
-        guardian1 = address(0x456);
-        guardian2 = address(0x789);
+        owner = 0x640E35B6AfC3F6AD37bC14792536dA06e1C8cc19;
+        guardian1 = 0xb2234c988cC1C4BE0b31fEbd2bA54BE61735f315;
+        guardian2 = 0x213ce1554f6F96dB5CdAEa7D750F89Ab2Ae43294;
         factory = new ETH_Factory(entryPoint);
         keycrypt = factory.createAccount(owner, guardian1, guardian2, 0);
     }
 
-    function test_ETH_KeycryptDotAddToWhitelist() public {
+    function test_ETH_KeycryptDotAddToWhitelist(bytes calldata _sign) public {
         vm.deal(address(keycrypt), 1 ether);
         console.log('keycrypt balance:', address(keycrypt).balance / 1e18);
         console.log('keycrypt owner:', keycrypt.owner());
@@ -37,6 +37,8 @@ contract KeycryptTest is Test {
         addresses.push(0x6B175474E89094C44Da98b954EedeAC495271d0F);
         //create calldata for addToWhitelist function
         bytes memory callData_ = abi.encodeWithSignature("addToWhitelist(address[])", addresses);
+        // console.log('callData:', callData_);
+        console.log('keycrypt address:', address(keycrypt));
         // create UserOperation struct
         UserOperation[] memory userOp;
         userOp[0] = UserOperation({
@@ -51,6 +53,7 @@ contract KeycryptTest is Test {
             maxPriorityFeePerGas: 60865874,
             paymasterAndData: bytes(""),
             // create signature signed by owner and guardian1, with signing data being userOp[0]
+            signature: _sign
         });
         entryPoint.handleOps(userOp, payable(msg.sender));
         addresses[0] = address(0x123);
